204e21a226a911cba953585f6ae12bf879d03f14
onlylosersleftalive <malaniko888@mail.ru>
2021-04-27 20:25:54 +0900

    add functionaliy to delete and complete buttons

--- app.js
+++ app.js
@@ -5,27 +5,43 @@ const toDoList = document.querySelector('.todo-list');
 
 //Event Listeners
 toDoButton.addEventListener('click', addToDo);
+toDoList.addEventListener('click', removeToDoItem);
 
 //Functions
-function addToDo(event){
+function addToDo(event) {
       event.preventDefault();
       
       const toDoDiv = document.createElement('div');
       toDoDiv.classList.add('todo');
-      const newToDo = document.createElement('li');
-      newToDo.innerText = 'hey';
-      newToDo.classList.add('todo-item');
-      toDoDiv.appendChild(newToDo);
+
+      const newToDoItem = document.createElement('li');
+      newToDoItem.innerText = toDoInput.value;
+      newToDoItem.classList.add('todo-item');
+      toDoDiv.appendChild(newToDoItem);
 
       const completedButton = document.createElement('button');
       completedButton.innerHTML = '<i class="fas fa-check"></i>';
-      completedButton.classList.add('complete-btn');
+      completedButton.classList.add('completed-btn');
       toDoDiv.appendChild(completedButton);
+
       const deleteButton = document.createElement('button');
       deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
       deleteButton.classList.add('delete-btn');
       toDoDiv.appendChild(deleteButton);
 
       toDoList.appendChild(toDoDiv);
+      toDoInput.value = "";
 
+}
+
+function removeToDoItem(event) {
+    const item = event.target;
+    if (item.classList[0] === "delete-btn") {
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    if (item.classList[0] === "completed-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed"); 
+    }
 }
