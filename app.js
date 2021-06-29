30b3703bf3ec61c017678e0c7e0ee4b17362afb5
Svetlana <68298554+onlylosersleftalive@users.noreply.github.com>
2021-06-29 17:28:41 +0900

    change functionality to prevent adding an empty string

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 //Selectors
-const toDoInput = document.querySelector('.todo-input');
-const toDoButton = document.querySelector('.todo-button');
-const toDoList = document.querySelector('.todo-list');
+const toDoInput = document.querySelector(".todo-input");
+const toDoButton = document.querySelector(".todo-button");
+const toDoList = document.querySelector(".todo-list");
 
 //Event Listeners
-toDoButton.addEventListener('click', addToDo);
-toDoList.addEventListener('click', removeToDoItem);
+toDoButton.addEventListener("click", addToDo);
+toDoList.addEventListener("click", removeToDoItem);
 
 //Functions
 function addToDo(event) {
-      event.preventDefault();
-      
-      const toDoDiv = document.createElement('div');
-      toDoDiv.classList.add('todo');
+  event.preventDefault();
 
-      const newToDoItem = document.createElement('li');
-      newToDoItem.innerText = toDoInput.value;
-      newToDoItem.classList.add('todo-item');
-      toDoDiv.appendChild(newToDoItem);
+  const toDoDiv = document.createElement("div");
+  toDoDiv.classList.add("todo");
 
-      const completedButton = document.createElement('button');
-      completedButton.innerHTML = '<i class="fas fa-check"></i>';
-      completedButton.classList.add('completed-btn');
-      toDoDiv.appendChild(completedButton);
+  if (toDoInput.value !== "") {
+    const newToDoItem = document.createElement("li");
+    newToDoItem.innerText = toDoInput.value;
+    newToDoItem.classList.add("todo-item");
+    toDoDiv.appendChild(newToDoItem);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("completed-btn");
+    toDoDiv.appendChild(completedButton);
 
-      const deleteButton = document.createElement('button');
-      deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
-      deleteButton.classList.add('delete-btn');
-      toDoDiv.appendChild(deleteButton);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="far fa-trash-alt"></i>';
+    deleteButton.classList.add("delete-btn");
+    toDoDiv.appendChild(deleteButton);
 
-      toDoList.appendChild(toDoDiv);
-      toDoInput.value = "";
+    toDoList.appendChild(toDoDiv);
+  } else {
+    alert("Add a task");
+  }
 
+  toDoInput.value = "";
 }
 
 function removeToDoItem(event) {
-    const item = event.target;
-    if (item.classList[0] === "delete-btn") {
-        const todo = item.parentElement;
-        todo.remove();
-    }
-    if (item.classList[0] === "completed-btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completed"); 
-    }
+  const item = event.target;
+  if (item.classList[0] === "delete-btn") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  if (item.classList[0] === "completed-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
